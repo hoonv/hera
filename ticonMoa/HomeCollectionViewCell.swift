e8c46b2683bf6098a33e68dd8710a8a824ee37e7
hoonv <chaeefe@gmail.com>
2021-03-25 14:29:31 +0900

    add Simple HomeCollectionView
    -

---
+++ ticonMoa/HomeCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  HomeCollectionViewCell.swift
+//  ticonMoa
+//
+//  Created by 채훈기 on 2021/03/25.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var image: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

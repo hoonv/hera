6e4b9aed9d5a0672655c31cdbe12534bc6f0cee0
hoonv <chaeefe@gmail.com>
2021-03-25 14:31:05 +0900

    apply Collectionview to main VC
    -

--- ticonMoa/ViewController.swift
+++ ticonMoa/ViewController.swift
@@ -12,8 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.collectionView.register(UINib(nibName: "HomeCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
     }
-
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? HomeCollectionViewCell else { return UICollectionViewCell() }
+        cell.image.image = images[indexPath.row]
+        return cell
+        
+    }
+    
+    
 }
-

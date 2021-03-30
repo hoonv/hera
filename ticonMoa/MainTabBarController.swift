fce9d94824b6d6cfc558d6ed87d2ddec03b32cde
hoonv <chaeefe@gmail.com>
2021-03-30 21:44:57 +0900

    add MainTabbarController
    - 메인 탭바 추가

---
+++ ticonMoa/MainTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  UITabBarController.swift
+//  ticonMoa
+//
+//  Created by 채훈기 on 2021/03/30.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    let floatingTabbarView = RoundTabBarView(["house", "magnifyingglass", "person"])
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.isHidden = true
+        setupFloatingTabBar()
+        self.viewControllers = []
+    }
+
+    func setupFloatingTabBar() {
+        floatingTabbarView.delegate = self
+        view.addSubview(floatingTabbarView)
+        floatingTabbarView.centerXInSuperview()
+        floatingTabbarView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+    }
+}
+
+extension MainTabBarController: RoundTabBarViewDelegate {
+    func roundTabBarView(_ roundTabBarView: RoundTabBarView, didSelected index: Int) {
+        self.selectedIndex = index
+    }
+    
+}

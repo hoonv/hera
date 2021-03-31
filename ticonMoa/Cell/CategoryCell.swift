f9ac4e0a26277bf3210baf72e94ce1ca0d5b6449
hoonv <chaeefe@gmail.com>
2021-03-31 16:17:17 +0900

    create CategoryCollectionViewCell
    -

---
+++ ticonMoa/Cell/CategoryCell.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryCell.swift
+//  ticonMoa
+//
+//  Created by 채훈기 on 2021/03/31.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        containerView.backgroundColor = .black
+        titleLabel.textColor = .white
+    }
+
+}

c6f258d5f5c3756573f73b55e0770dacfa601e5e
hoonv <chaeefe@gmail.com>
2021-03-31 16:35:11 +0900

    create Extension for estimate string width height
    -

---
+++ ticonMoa/Extensions/String+widthHeight.swift
@@ -0,0 +1,28 @@
+//
+//  String+widthHeight.swift
+//  ticonMoa
+//
+//  Created by 채훈기 on 2021/03/31.
+//
+
+import UIKit
+
+extension String {
+
+    func widthOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.width
+    }
+
+    func heightOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.height
+    }
+
+    func sizeOfString(usingFont font: UIFont) -> CGSize {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        return self.size(withAttributes: fontAttributes)
+    }
+}

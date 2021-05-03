bf9d3e1dfec92deab8543ba98bac57b26244ab67
hoonv <chaeefe@gmail.com>
2021-05-03 11:22:01 +0900

    move code that request logic to VC
    -

--- ticonMoa/HomeScene/Controller/HomeViewController.swift
+++ ticonMoa/HomeScene/Controller/HomeViewController.swift
@@ -43,48 +43,23 @@ class HomeViewController: UIViewController {
         
         super.viewDidLoad()
     }
-    
-    private func setupUI() {
-        self.view.layer.cornerRadius = 30
-        self.iconStackView.delegate = self
-
-        self.collectionView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
-        self.collectionView.layer.cornerRadius = 30
-        self.collectionView.register(UINib(nibName: "HomeCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
-        
-        let panRight = UIPanGestureRecognizer(target: self, action: #selector(handleGesture(_:)))
-        view.addGestureRecognizer(panRight)
-
-        indicator.centerInSuperview()
+   
+    func showAuto() {
+        viewModel.input.requestPhotoWithAuto()
     }
     
-    @objc func handleGesture(_ gesture: UIPanGestureRecognizer) {
-        let translate = gesture.translation(in: gesture.view)
-        let percent   = translate.x / gesture.view!.bounds.size.width
+    func showManual() {
+        imagePickerController.allowsEditing = false
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
         
-        if gesture.state == .began {
-            let controller = storyboard!.instantiateViewController(withIdentifier: "PhotoAddViewController") as! MapViewController
-            interactionController = UIPercentDrivenInteractiveTransition()
-            controller.customTransitionDelegate.interactionController = interactionController
-
-            show(controller, sender: self)
-        } else if gesture.state == .changed {
-            interactionController?.update(percent * 0.8)
-        } else if gesture.state == .ended || gesture.state == .cancelled {
-            let velocity = gesture.velocity(in: gesture.view)
-            if (percent > 0.5 && velocity.x == 0) || velocity.x > 100 {
-                interactionController?.finish()
-            } else {
-                interactionController?.cancel()
-            }
-            interactionController = nil
-        }
+        present(imagePickerController, animated: true, completion: nil)
     }
-}
-
-extension HomeViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let w = (self.view.frame.width - 20) / 3
-        return CGSize(width: w, height: w)
+    
+    func presentManualViewController(image: UIImage?) {
+        guard let controller: ManualPhotoViewController = UIStoryboard.main.instantiate() else { return }
+        controller.selectedImage = image
+        self.show(controller, sender: self)
     }
+    
 }

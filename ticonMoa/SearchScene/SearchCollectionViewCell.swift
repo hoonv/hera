ef5e792e8bb8c751f4f487b88fa35316fbf2d25b
hoonv <chaeefe@gmail.com>
2021-05-03 11:18:09 +0900

    Create SearchVC
    -

---
+++ ticonMoa/SearchScene/SearchCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SearchCollectionViewCell.swift
+//  ticonMoa
+//
+//  Created by 채훈기 on 2021/05/02.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

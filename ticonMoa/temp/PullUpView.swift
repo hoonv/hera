00ead98144aebef12d8145d758721b350e69d36d
hoonv <chaeefe@gmail.com>
2021-05-06 08:59:35 +0900

    Create PullUPView
    -

---
+++ ticonMoa/temp/PullUpView.swift
@@ -0,0 +1,20 @@
+//
+//  PullUpView.swift
+//  ticonMoa
+//
+//  Created by 채훈기 on 2021/05/06.
+//
+
+import UIKit
+
+class PullUpView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

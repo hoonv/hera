8ade93c318b8cadd1124a4b9455049b16920d29b
hoonv <chaeefe@gmail.com>
2021-05-10 20:06:57 +0900

    update Recognizer
    -

---
+++ ticonMoa/ManualAddingScene/Model/NameRecognizer.swift
@@ -0,0 +1,15 @@
+//
+//  NameRecognizer.swift
+//  ticonMoa
+//
+//  Created by 채훈기 on 2021/05/10.
+//
+
+import Foundation
+
+class NameRecognizer {
+    
+    func match(input: [String]) -> String? {
+        
+    }
+}

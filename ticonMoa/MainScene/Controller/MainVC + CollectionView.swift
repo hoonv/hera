8255d59d24e4585eb121428f873e8c202926a268
hoonv <chaeefe@gmail.com>
2021-05-11 15:44:57 +0900

    update Main Cell
    -

--- ticonMoa/MainScene/Controller/MainVC + CollectionView.swift
+++ ticonMoa/MainScene/Controller/MainVC + CollectionView.swift
@@ -16,12 +16,22 @@ extension MainViewController {
 }
 
 extension MainViewController: UICollectionViewDelegateFlowLayout {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 0, bottom: 30, right: 0)
+    }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: (view.frame.width - 50) / 2, height: 220)
+        return CGSize(width: (view.frame.width - 50), height: 160)
     }
 }
 
 extension MainViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
     }
@@ -29,23 +39,18 @@ extension MainViewController: UICollectionViewDataSource, UICollectionViewDelega
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainCollectionViewCell", for: indexPath) as? MainCollectionViewCell else { return UICollectionViewCell() }
         cell.imageView.image = images[indexPath.row]
-        cell.layer.cornerRadius = 12
+        cell.layer.cornerRadius = 30
         return cell
-    }
-    
-    
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: view.frame.width - 40, height: 64)
-    }
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "MainCategoryHeaderView", for: indexPath) as? MainCategoryHeaderView else { return UICollectionReusableView() }
         return header
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width - 40, height: 64)
     }
 }
 
--- ticonMoa/MainScene/View/Cell/MainCollectionViewCell.swift
+++ ticonMoa/MainScene/View/Cell/MainCollectionViewCell.swift
@@ -11,18 +11,12 @@ class MainCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var imageView: UIImageView!
     
-    @IBOutlet weak var bottomView: UIView!
-    
     @IBOutlet weak var date: UILabel!
     @IBOutlet weak var brand: UILabel!
     @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var moveButton: UIButton!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        imageView.layer.cornerRadius = 12
-        bottomView.layer.cornerRadius = 12
-        moveButton.layer.cornerRadius = 8
     }
 
 }
